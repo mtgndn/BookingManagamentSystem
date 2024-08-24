2f70fea9051e9baab0c748a67346db4c7b51dbd4
mg42otw <mtgndn@gmail.com>
2024-08-24 15:36:01 +0300

    Nextjs

---
+++ NextjsProject/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Genius",
+  description: "AI platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    
+      <html lang="en">
+        <body>
+          <SignedOut>
+
+            <SignInButton/>
+
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
+           {children}
+        </body>
+      </html>
+
+    </ClerkProvider>
+  
+  );
+}
